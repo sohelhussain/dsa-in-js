427688fe2a0c61b16b2e4f7c2ac4d5b0913149e4
sohelhussain <sohelhussaing@gmail.com>
2024-04-14 11:21:49 +0530

    update:- opt-sum_zero, and write stack

--- suneja/sumzero.js
+++ suneja/sumzero.js
@@ -30,18 +30,23 @@ for (let i = 0; i < arr.length; i++) {
 
 const arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
 
-for(let i = 0; i < arr.length; i++){
-    if(arr[i] > 0){
-        for(let j = arr.length; j >= 0; j--){
-            if(arr[i] + arr[j] === 0) {
-                console.log(arr[i], arr[j]);
-            }
+
+
+const optimizer = arr => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        }else if(sum > 0) {
+            right--
+        }else{
+            left++;
         }
     }
 }
-
-
-
-
-
+let ans = optimizer(arr);
+console.log(ans);
 
